a3b312441e2f36500b3ab62d5cf8592d08f5d42c
Christopher Brown <chrisjosbrown@gmail.com>
2024-05-12 20:45:46 +0200

    add ammo type

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -48,3 +48,86 @@ BOILERPLATE.abilityAbbreviations = {
 	agi: 'Agi',
 	lck: 'Lck',
 };
+
+BOILERPLATE.ammoTypes = [
+	{
+		name: "9mm",
+		type: "9mm",
+	},
+	{
+		name: "5mm",
+		type: "5mm",
+	},
+	{
+		name: ".32",
+		type: "32"
+	},
+	{
+		name: "20 gauge",
+		type: "20_gauge"
+	},
+	{
+		name: "12 gauge",
+		type: "12_gauge"
+	},
+	{
+		name: "5.56",
+		type: "556"
+	},
+	{
+		name: "10mm",
+		type: "10mm"
+	},
+	{
+		name: ".357",
+		type: "357"
+	},
+	{
+		name: ".45",
+		type: "45"
+	},
+	{
+		name: "Tank of Flamer Fuel",
+		type: "tank_of_flamer_fuel"
+	},
+	{
+		name: ".44",
+		type: "44"
+	},
+	{
+		name: "Energy Cells",
+		type: "energy_cells"
+	},
+	{
+		name: "Microfusion Cells",
+		type: "microfusion_cells"
+	},
+	{
+		name: ".45-70 Gov't.",
+		type: "45_70_govt"
+	},
+	{
+		name: ".308",
+		type: "308"
+	},
+	{
+		name: ".50",
+		type: "50"
+	},
+	{
+		name: "2mm EC",
+		type: "2mm_ec"
+	},
+	{
+		name: "Fusion Core",
+		type: "fusion_core"
+	},
+	{
+		name: "Missiles",
+		type: "missiles"
+	},
+	{
+		name: "Mini-Nuke",
+		type: "mini_nuke"
+	},
+]
