4961bfccff0460f620d96881a012bab7f225789b
Christopher Brown <chrisjosbrown@gmail.com>
2024-05-14 23:30:18 +0200

    filter out ammos that don't match weapon type

--- module/data/ammo.mjs
+++ module/data/ammo.mjs
@@ -7,10 +7,11 @@ export default class FalloutZeroItemAmmo extends FalloutZeroItemBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
+    schema.img = new fields.StringField({ initial: "systems/arcane-arcade-fallout/assets/vaultboy/ranged-weapon-icon.webp" })
     schema.quantity = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
     schema.load = new fields.NumberField({ required: true, nullable: false, initial: .1, min: 0 });
     schema.cost = new fields.NumberField({ required: true, nullable: false, initial: 1, min: 0 });
-    schema.img = new fields.StringField({ initial: "systems/arcane-arcade-fallout/assets/vaultboy/ranged-weapon-icon.webp" })
+    schema.type = new fields.StringField({ initial: "9mm" })
 
     return schema;
   }
