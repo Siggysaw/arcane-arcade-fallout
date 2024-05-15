afe36bfcf46bc88076cf81cf3bc92600f22f6843
Siggysaw <168799854+Siggysaw@users.noreply.github.com>
2024-05-15 02:05:23 -0400

    First NPC Created

--- module/data/npc.mjs
+++ module/data/npc.mjs
@@ -1,19 +1,62 @@
 import FalloutZeroActorBase from "./actorBase.mjs";
+import { FALLOUTZERO } from "../config.mjs";
 
-export default class FalloutZeroNPC extends FalloutZeroActorBase {
+export default class FalloutZeroCharacter extends FalloutZeroActorBase {
 
   static defineSchema() {
     const fields = foundry.data.fields;
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.cr = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
-    schema.xp = new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 });
-    
-    return schema
+    schema.level = new fields.NumberField({
+      ...requiredInteger,
+      initial: 1,
+      min: 1,
+      max: 30
+    })
+
+    schema.penalties = new fields.SchemaField(Object.keys(FALLOUTZERO.penalties).reduce((obj, penalty) => {
+      obj[penalty] = new fields.SchemaField({
+        label: new fields.StringField({ required: true }),
+        value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+      });
+      return obj;
+    }, {}))
+
+    schema.carryLoad = new fields.SchemaField({
+      value: new fields.NumberField({
+        ...requiredInteger,
+        initial: 0,
+      }),
+      min: new fields.NumberField({
+        ...requiredInteger,
+        initial: 0
+      }),
+      max: new fields.NumberField({
+        ...requiredInteger,
+        initial: 0,
+      })
+    })
+    schema.background = new fields.StringField({ initial: "", blank: true })
+    schema.race = new fields.StringField({ initial: "", blank: true })
+    schema.xp = new fields.NumberField({ initial: 0, min: 0 })
+    schema.combatSequence = new fields.NumberField({ initial: 0, min: 0 })	
+    schema.healingRate = new fields.NumberField({ initial: 0, min: 0 })
+    schema.passiveSense = new fields.NumberField({
+      ...requiredInteger,
+      initial: 0
+    })
+    schema.penaltyTotal = new fields.NumberField({ initial: 0, min: 0 })
+	schema.properties = new fields.HTMLField();
+
+    return schema;
   }
 
-  prepareDerivedData() {
-    this.xp = this.cr * this.cr * 100;
+  prepareBaseData() {
+    super.prepareBaseData()
+    for (const key in this.penalties) {
+      this.penalties[key].label = FALLOUTZERO.penalties[key]
+    }
+    this.passiveSense = 12 + this.abilities['per'].mod
   }
 }
