232f2c704234106cdcc46214a3a9efb726dea11d
Siggysaw <168799854+Siggysaw@users.noreply.github.com>
2024-05-16 00:59:29 -0400

    Updated Weapons Table To utilize Buttons

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -11,7 +11,7 @@ export const preloadHandlebarsTemplates = async function () {
     'systems/arcane-arcade-fallout/templates/actor/parts/actor-items.hbs',
     'systems/arcane-arcade-fallout/templates/actor/parts/actor-effects.hbs',
     'systems/arcane-arcade-fallout/templates/actor/parts/actor-perks.hbs',
-	'systems/arcane-arcade-fallout/templates/actor/parts/actor-armor.hbs',
+	'systems/arcane-arcade-fallout/templates/actor/parts/actor-armors.hbs',
 	'systems/arcane-arcade-fallout/templates/actor/parts/npc-status.hbs', 
 
     // Item partials
