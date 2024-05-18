f61297c26e1599b07a50b509c17be624f1bea0a9
Christopher Brown <chrisjosbrown@gmail.com>
2024-05-18 10:01:34 +0200

    calc skill roll correctly

--- module/data/actorBase.mjs
+++ module/data/actorBase.mjs
@@ -1,12 +1,11 @@
-import { FALLOUTZERO } from "../config.mjs";
+import { FALLOUTZERO } from '../config.mjs'
 
-export default class FalloutZeroActorBase extends foundry.abstract
-  .TypeDataModel {
+export default class FalloutZeroActorBase extends foundry.abstract.TypeDataModel {
   static defineSchema() {
-    const fields = foundry.data.fields;
-    const requiredInteger = { required: true, nullable: false, integer: true };
-    const schema = {};
-    schema.biography = new fields.HTMLField();
+    const fields = foundry.data.fields
+    const requiredInteger = { required: true, nullable: false, integer: true }
+    const schema = {}
+    schema.biography = new fields.HTMLField()
     schema.health = new fields.SchemaField({
       value: new fields.NumberField({
         ...requiredInteger,
@@ -20,7 +19,7 @@ export default class FalloutZeroActorBase extends foundry.abstract
         ...requiredInteger,
         initial: 10,
       }),
-    });
+    })
     schema.stamina = new fields.SchemaField({
       value: new fields.NumberField({
         ...requiredInteger,
@@ -35,7 +34,7 @@ export default class FalloutZeroActorBase extends foundry.abstract
         ...requiredInteger,
         initial: 10,
       }),
-    });
+    })
     schema.actionPoints = new fields.SchemaField({
       value: new fields.NumberField({
         ...requiredInteger,
@@ -49,7 +48,7 @@ export default class FalloutZeroActorBase extends foundry.abstract
         ...requiredInteger,
         initial: 10,
       }),
-    });
+    })
     schema.karmaCaps = new fields.SchemaField({
       value: new fields.NumberField({
         ...requiredInteger,
@@ -63,7 +62,7 @@ export default class FalloutZeroActorBase extends foundry.abstract
         ...requiredInteger,
         initial: 1,
       }),
-    });
+    })
 
     // Iterate over ability names and create a new SchemaField for each.
     schema.abilities = new fields.SchemaField(
@@ -82,23 +81,16 @@ export default class FalloutZeroActorBase extends foundry.abstract
           label: new fields.StringField({
             initial: FALLOUTZERO.abilities[ability].label,
           }),
-        });
-        return obj;
-      }, {})
-    );
+        })
+        return obj
+      }, {}),
+    )
 
     schema.skills = new fields.SchemaField(
       Object.keys(FALLOUTZERO.skills).reduce((obj, skill) => {
         obj[skill] = new fields.SchemaField({
-          ability: new fields.ArrayField(
-            new fields.StringField({ required: true })
-          ),
+          ability: new fields.ArrayField(new fields.StringField({ required: true })),
           value: new fields.NumberField({
-            ...requiredInteger,
-            initial: 0,
-            min: 0,
-          }),
-          mod: new fields.NumberField({
             ...requiredInteger,
             initial: 0,
             min: 0,
@@ -106,10 +98,10 @@ export default class FalloutZeroActorBase extends foundry.abstract
           label: new fields.StringField({
             initial: FALLOUTZERO.skills[skill].label,
           }),
-        });
-        return obj;
-      }, {})
-    );
+        })
+        return obj
+      }, {}),
+    )
 
     schema.armorClass = new fields.SchemaField({
       value: new fields.NumberField({
@@ -120,7 +112,7 @@ export default class FalloutZeroActorBase extends foundry.abstract
         ...requiredInteger,
         initial: 0,
       }),
-    });
+    })
 
     schema.damageThreshold = new fields.SchemaField({
       value: new fields.NumberField({
@@ -131,165 +123,154 @@ export default class FalloutZeroActorBase extends foundry.abstract
         ...requiredInteger,
         initial: 0,
       }),
-    });
+    })
 
     schema.caps = new fields.NumberField({
       initial: 0,
       min: 0,
-    });
+    })
 
-    return schema;
+    return schema
   }
 
   prepareBaseData() {
-    super.prepareBaseData();
+    super.prepareBaseData()
     // Loop through ability scores, and add their modifiers to our sheet output.
     for (const key in this.abilities) {
       // Calculate the modifier using d20 rules.
-      this.abilities[key].mod = Math.floor(this.abilities[key].value - 5);
+      this.abilities[key].mod = Math.floor(this.abilities[key].value - 5)
     }
 
     // Loop through skill scores, and add their modifiers to our sheet output.
     for (const key in this.skills) {
-      // Calculate the modifier using d20 rules.
-      this.skills[key].mod = Math.floor(this.skills[key].value);
-      this.skills[key].ability = FALLOUTZERO.skills[key].ability;
+      this.skills[key].ability = FALLOUTZERO.skills[key].ability
     }
   }
 
   rollWeapon(weaponId, hasDisadvantage = false) {
-    const currentAp = this.actionPoints.value;
-    const weapon = this.parent.items.get(weaponId);
-    const apCost = weapon.system.apCost;
-    const newAP = Number(currentAp) - Number(apCost);
+    const currentAp = this.actionPoints.value
+    const weapon = this.parent.items.get(weaponId)
+    const apCost = weapon.system.apCost
+    const newAP = Number(currentAp) - Number(apCost)
 
     // if action would reduce AP below 0
     if (newAP < 0) {
-      ui.notifications.warn(`Not enough AP for action`);
-      return;
+      ui.notifications.warn(`Not enough AP for action`)
+      return
     }
 
     // if weapon ammo capacity is 0
     if (weapon.system.ammo.capacity.value < 1) {
-      ui.notifications.warn(`Weapon ammo is empty, need to reload`);
-      return;
+      ui.notifications.warn(`Weapon ammo is empty, need to reload`)
+      return
     }
 
     // Update ammo quantity
-    const foundAmmo = this.parent.items.get(weapon.system.ammo.consumes.target);
+    const foundAmmo = this.parent.items.get(weapon.system.ammo.consumes.target)
     if (foundAmmo) {
-      const newAmmoQty = Number(foundAmmo.system.quantity - 1);
-      const newWeaponAmmoCapacity = Number(
-        weapon.system.ammo.capacity.value - 1
-      );
-      this.parent.updateEmbeddedDocuments("Item", [
-        { _id: foundAmmo._id, "system.quantity": newAmmoQty },
+      const newAmmoQty = Number(foundAmmo.system.quantity - 1)
+      const newWeaponAmmoCapacity = Number(weapon.system.ammo.capacity.value - 1)
+      this.parent.updateEmbeddedDocuments('Item', [
+        { _id: foundAmmo._id, 'system.quantity': newAmmoQty },
         {
           _id: weapon._id,
-          "system.ammo.capacity.value": newWeaponAmmoCapacity,
+          'system.ammo.capacity.value': newWeaponAmmoCapacity,
         },
-      ]);
+      ])
 
-      console.log("ACTOR UPDATE", {
-        "item.system.quantity": newAmmoQty,
-        "weapon.system.ammo.capacity.value": newWeaponAmmoCapacity,
-      });
+      console.log('ACTOR UPDATE', {
+        'item.system.quantity': newAmmoQty,
+        'weapon.system.ammo.capacity.value': newWeaponAmmoCapacity,
+      })
     }
 
     // update actor AP
-    this.parent.update({ "system.actionPoints.value": Number(newAP) });
+    this.parent.update({ 'system.actionPoints.value': Number(newAP) })
 
     // roll to hit
-    const dice = hasDisadvantage ? "2d20kl" : "d20";
+    const dice = hasDisadvantage ? '2d20kl' : 'd20'
     let roll = new Roll(
       `${dice} + ${this.skills[weapon.system.skillBonus].value} + ${this.abilities[weapon.system.abilityMod].mod} - ${this.penaltyTotal}`,
-      this.getRollData()
-    );
+      this.getRollData(),
+    )
     roll.toMessage({
       speaker: ChatMessage.getSpeaker({ actor: this.parent }),
       flavor: `BOOM! Attack with a ${weapon.name}`,
-      rollMode: game.settings.get("core", "rollMode"),
-    });
+      rollMode: game.settings.get('core', 'rollMode'),
+    })
 
-    console.log("ACTOR UPDATE", {
-      "system.actionPoints.value": newAP,
-    });
+    console.log('ACTOR UPDATE', {
+      'system.actionPoints.value': newAP,
+    })
 
-    return roll;
+    return roll
   }
 
   getWeaponsNewCapacity(weapon, consumableAmmo) {
-    if (
-      consumableAmmo &&
-      consumableAmmo.system.quantity < weapon.system.ammo.capacity.max
-    ) {
-      return consumableAmmo.system.quantity;
+    if (consumableAmmo && consumableAmmo.system.quantity < weapon.system.ammo.capacity.max) {
+      return consumableAmmo.system.quantity
     } else {
-      return weapon.system.ammo.capacity.max;
+      return weapon.system.ammo.capacity.max
     }
   }
 
   reload(weaponId = null) {
-    const weapon = this.parent.items.get(weaponId);
+    const weapon = this.parent.items.get(weaponId)
     if (!weapon) {
-      ui.notifications.warn(`Weapon ${weaponId} not found on actor`);
-      return;
+      ui.notifications.warn(`Weapon ${weaponId} not found on actor`)
+      return
     }
 
-    const newAP = this.actionPoints.value - 6;
+    const newAP = this.actionPoints.value - 6
     if (newAP < 0) {
-      ui.notifications.warn(`Not enough action points to reload`);
-      return;
+      ui.notifications.warn(`Not enough action points to reload`)
+      return
     }
 
     if (weapon.system.capacityAtMax) {
-      ui.notifications.warn(`Weapon capacity is already at max`);
-      return;
+      ui.notifications.warn(`Weapon capacity is already at max`)
+      return
     }
 
-    const consumableAmmo = this.parent.items.get(
-      weapon.system.ammo.consumes.target
-    );
+    const consumableAmmo = this.parent.items.get(weapon.system.ammo.consumes.target)
     if (!consumableAmmo) {
-      ui.notifications.warn(
-        `No rounds assigned to weapon, set a consumable ammo`
-      );
-      return;
+      ui.notifications.warn(`No rounds assigned to weapon, set a consumable ammo`)
+      return
     } else if (
       consumableAmmo.system.quantity < 1 ||
       consumableAmmo.system.quantity === weapon.system.ammo.capacity.value
     ) {
-      ui.notifications.warn(`No rounds left to reload weapon`);
-      return;
+      ui.notifications.warn(`No rounds left to reload weapon`)
+      return
     }
 
-    const newCapacity = this.getWeaponsNewCapacity(weapon, consumableAmmo);
+    const newCapacity = this.getWeaponsNewCapacity(weapon, consumableAmmo)
     if (newCapacity < 1) {
-      ui.notifications.warn(`No rounds left to reload weapon`);
-      return;
+      ui.notifications.warn(`No rounds left to reload weapon`)
+      return
     }
 
-    this.parent.update({ "system.actionPoints.value": newAP });
-    this.parent.updateEmbeddedDocuments("Item", [
-      { _id: weaponId, "system.ammo.capacity.value": newCapacity },
-    ]);
+    this.parent.update({ 'system.actionPoints.value': newAP })
+    this.parent.updateEmbeddedDocuments('Item', [
+      { _id: weaponId, 'system.ammo.capacity.value': newCapacity },
+    ])
   }
 
   getRollData() {
-    const data = {};
+    const data = {}
 
     // Copy the ability scores to the top level, so that rolls can use
     // formulas like `@str.mod + 4`.
     if (this.abilities) {
       for (let [k, v] of Object.entries(this.abilities)) {
-        data[k] = foundry.utils.deepClone(v);
+        data[k] = foundry.utils.deepClone(v)
       }
     }
 
     if (this?.attributes?.level?.value) {
-      data.lvl = this.attributes.level.value;
+      data.lvl = this.attributes.level.value
     }
 
-    return data;
+    return data
   }
 }
