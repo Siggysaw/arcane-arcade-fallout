e5134ef663113ae79cc8d082ffb16ac40e23c515
Siggysaw <168799854+Siggysaw@users.noreply.github.com>
2024-05-18 23:14:00 -0400

    Added Junk Type and added to backpack

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -94,6 +94,7 @@ export class FalloutZeroActorSheet extends ActorSheet {
     const medicines = []
     const foodAnddrinks = []
     const ammos = []
+    const junk = []	
 
     // Iterate through items, allocating to containers
     for (let i of context.items) {
@@ -116,7 +117,9 @@ export class FalloutZeroActorSheet extends ActorSheet {
         foodAnddrinks.push(i)
       } else if (i.type === 'ammo') {
         ammos.push(i)
-      }
+      } else if (i.type === 'junkItem') {
+        junk.push(i)
+      }  
     }
 
     // Assign and return
@@ -127,6 +130,7 @@ export class FalloutZeroActorSheet extends ActorSheet {
     context.medicines = medicines
     context.foodAnddrinks = foodAnddrinks
     context.ammos = ammos
+    context.junk = junk	
     context.rangedWeapons = rangedWeapons.map((weapon) => {
       weapon.ammos = ammos.filter((ammo) => ammo.system.type === weapon.system.ammo.type)
       // if (!weapon.system.range.flat) {
