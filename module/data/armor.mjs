2e1097f8985bf143192a66d7c2c644b869f11edc
Siggysaw <168799854+Siggysaw@users.noreply.github.com>
2024-05-24 17:53:51 -0400

    Updated Item Sheet / Added Condition to Armor
    Weird issue where the default isn't coming through though

---
+++ module/data/armor.mjs
@@ -0,0 +1,20 @@
+import { FalloutZeroWeapon } from './_module.mjs'
+
+export default class FalloutZeroRangedWeapon extends FalloutZeroWeapon {
+  static defineSchema() {
+    const fields = foundry.data.fields
+    const schema = super.defineSchema()
+
+    schema.abilityMod = new fields.StringField({ initial: 'agi' })
+    schema.upgrades = new fields.HTMLField({})	
+    schema.skillBonus = new fields.StringField({ initial: 'guns' })
+    schema.consumesAmmo = new fields.BooleanField({ initial: true })
+    schema.decay = new fields.NumberField({ initial:10, min:0, max:10})	
+	
+    return schema
+  }
+
+  prepareDerivedData() {
+  
+  }
+}
