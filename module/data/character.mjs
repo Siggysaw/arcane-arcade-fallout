3efdf21dc4e6b13ef699479f5754c89ed6371427
Siggysaw <168799854+Siggysaw@users.noreply.github.com>
2024-05-29 17:25:18 -0400

    Added Backpack to NPCs

--- module/data/character.mjs
+++ module/data/character.mjs
@@ -48,6 +48,7 @@ export default class FalloutZeroCharacter extends FalloutZeroActorBase {
     schema.groupSneak = new fields.NumberField({ initial: 0, min: 0 })
     schema.combatSequence = new fields.NumberField({ initial: 0, min: 0 })
     schema.partyNerve = new fields.NumberField({ initial: 0, min: 0 })
+    schema.irradiated = new fields.NumberField({ initial: 0, min: 0 })
     schema.passiveSense = new fields.NumberField({
       ...requiredInteger,
       initial: 0,
