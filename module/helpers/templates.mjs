b28c5106d5bfc7cc83e1d3284f79cf4a6a19a89a
Christopher Brown <chrisjosbrown@gmail.com>
2024-06-23 19:31:55 +0200

    chore(): separate penalties

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -31,6 +31,7 @@ export const preloadHandlebarsTemplates = async function () {
     // Actor Status
     'systems/arcane-arcade-fallout/templates/actor/status/special.hbs',
     'systems/arcane-arcade-fallout/templates/actor/status/skills.hbs',
+    'systems/arcane-arcade-fallout/templates/actor/status/penalties.hbs',
 
     // Item partials
     'systems/arcane-arcade-fallout/templates/item/parts/item-effects.hbs',
